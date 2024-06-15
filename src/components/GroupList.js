c1c5219a7faf73c35e79888b83590042cbfe5fd1
Satyaprakash Singh <107452937+Satya057@users.noreply.github.com>
2024-06-15 15:45:55 +0530

    done

--- src/components/GroupList.js
+++ src/components/GroupList.js
@@ -1,55 +1,3 @@
-// import React from 'react';
-// import { useDispatch, useSelector } from 'react-redux';
-// import { addGroup, setStatuses } from '../store';
-// import Group from './Group';
-// import axios from 'axios';
-
-// const GroupList = () => {
-//   const dispatch = useDispatch();
-//   const groups = useSelector((state) => state.groups);
-
-//   const handleAddGroup = () => {
-//     const lastGroup = groups[groups.length - 1];
-//     const nextFrom = lastGroup.to + 1;
-//     const nextTo = nextFrom + 2 <= 10 ? nextFrom + 2 : 10;
-//     if (nextFrom <= 10) {
-//       dispatch(addGroup({ from: nextFrom, to: nextTo }));
-//     }
-//   };
-
-//   const handleShowStatus = async () => {
-//     const allStatuses = await Promise.all(
-//       groups.map(async (group, index) => {
-//         const groupStatuses = {};
-//         for (let i = group.from; i <= group.to; i++) {
-//           const response = await axios.get(`https://jsonplaceholder.typicode.com/todos/${i}`);
-//           groupStatuses[i] = response.data.completed;
-//         }
-//         return { index, statuses: groupStatuses };
-//       })
-//     );
-
-//     allStatuses.forEach(({ index, statuses }) => {
-//       dispatch(setStatuses(index, statuses));
-//     });
-//   };
-
-//   return (
-//     <div className="group-list">
-//       {groups.map((group, index) => (
-//         <div key={index} className="group-container">
-//           <Group index={index} group={group} />
-//         </div>
-//       ))}
-//       <button className="show-status" onClick={handleShowStatus}>Show Status</button>
-//       <button className="add-group" onClick={handleAddGroup}>+ Add Group</button>
-//     </div>
-//   );
-// };
-
-// export default GroupList;
-
-
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { addGroup, setStatuses } from '../store';
@@ -60,6 +8,7 @@ const GroupList = () => {
   const dispatch = useDispatch();
   const groups = useSelector((state) => state.groups);
   const [allTasksCovered, setAllTasksCovered] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
 
   useEffect(() => {
     checkAllTasksCovered();
@@ -76,6 +25,11 @@ const GroupList = () => {
   };
 
   const handleShowStatus = async () => {
+    if (!allTasksCovered) {
+      setErrorMessage('You have not added all tasks from 1 to 10.');
+      return;
+    }
+
     const allStatuses = await Promise.all(
       groups.map(async (group, index) => {
         const groupStatuses = {};
@@ -90,6 +44,7 @@ const GroupList = () => {
     allStatuses.forEach(({ index, statuses }) => {
       dispatch(setStatuses(index, statuses));
     });
+    setErrorMessage(''); // Clear error message after successful status fetch
   };
 
   const checkAllTasksCovered = () => {
@@ -109,12 +64,14 @@ const GroupList = () => {
           <Group index={index} group={group} />
         </div>
       ))}
+      {errorMessage && <div className="error-message">{errorMessage}</div>}
       <button
-        className="show-status"
+        className={`show-status ${!allTasksCovered ? 'disabled' : ''}`}
         onClick={handleShowStatus}
         disabled={!allTasksCovered}
       >
         Show Status
+        {!allTasksCovered && <span className="tooltip">You have not added all tasks from 1 to 10.</span>}
       </button>
       <button className="add-group" onClick={handleAddGroup}>+ Add Group</button>
     </div>
