508d7d6af4e1b9154b7ddfd552094ee8e4997802
Satyaprakash Singh <107452937+Satya057@users.noreply.github.com>
2024-06-15 14:57:03 +0530

    done

--- src/components/Group.js
+++ src/components/Group.js
@@ -1,84 +1,10 @@
  
 
-// import React from 'react';
-// import { useDispatch, useSelector } from 'react-redux';
-// import { removeGroup, updateGroup, setStatuses } from '../store';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faTrash, faArrowRight } from '@fortawesome/free-solid-svg-icons';
-// import axios from 'axios';
-
-// const Group = ({ index, group }) => {
-//   const dispatch = useDispatch();
-//   const statuses = useSelector((state) => state.statuses[index]);
-
-//   const handleRemove = () => {
-//     dispatch(removeGroup(index));
-//   };
-
-//   const handleChange = (e) => {
-//     const { name, value } = e.target;
-//     dispatch(updateGroup(index, { ...group, [name]: Number(value) }));
-//   };
-
-//   const handleShowStatus = async () => {
-//     const groupStatuses = {};
-//     for (let i = group.from; i <= group.to; i++) {
-//       const response = await axios.get(`https://jsonplaceholder.typicode.com/todos/${i}`);
-//       groupStatuses[i] = response.data.completed;
-//     }
-//     dispatch(setStatuses(index, groupStatuses));
-//   };
-
-//   return (
-//     <div className="group">
-//       <div className="group-content">
-//         <button onClick={handleRemove} className="delete-button">
-//           <FontAwesomeIcon icon={faTrash} />
-//         </button>
-//         <div className="group-details">
-//           <span className="group-label">Group {index + 1}</span>
-//           <div className="group-inputs">
-//             <label>From:</label>
-//             <input
-//               type="number"
-//               name="from"
-//               value={group.from}
-//               onChange={handleChange}
-//               min="1"
-//               max="10"
-//             />
-//             <FontAwesomeIcon icon={faArrowRight} className="arrow-icon" />
-//             <label>To:</label>
-//             <input
-//               type="number"
-//               name="to"
-//               value={group.to}
-//               onChange={handleChange}
-//               min="1"
-//               max="10"
-//             />
-//           </div>
-//         </div>
-//         <div className="status-list">
-//           {statuses && Object.entries(statuses).map(([key, value]) => (
-//             <div key={key} className="status-item">
-//               ({key}) {value ? 'True' : 'False'}
-//             </div>
-//           ))}
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Group;
-
-
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { removeGroup, updateGroup, setStatuses } from '../store';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTrash, faArrowRight, faCheck } from '@fortawesome/free-solid-svg-icons';
+import { faTrash, faArrowRight } from '@fortawesome/free-solid-svg-icons';
 import axios from 'axios';
 
 const Group = ({ index, group }) => {
@@ -139,7 +65,6 @@ const Group = ({ index, group }) => {
               ({key}) {value ? 'True' : 'False'}
             </div>
           ))}
-          <FontAwesomeIcon icon={faCheck} className="check-icon" />
         </div>
       </div>
     </div>
@@ -147,3 +72,6 @@ const Group = ({ index, group }) => {
 };
 
 export default Group;
+
+
+ 
--- src/components/GroupList.js
+++ src/components/GroupList.js
@@ -1,4 +1,56 @@
-import React from 'react';
+// import React from 'react';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { addGroup, setStatuses } from '../store';
+// import Group from './Group';
+// import axios from 'axios';
+
+// const GroupList = () => {
+//   const dispatch = useDispatch();
+//   const groups = useSelector((state) => state.groups);
+
+//   const handleAddGroup = () => {
+//     const lastGroup = groups[groups.length - 1];
+//     const nextFrom = lastGroup.to + 1;
+//     const nextTo = nextFrom + 2 <= 10 ? nextFrom + 2 : 10;
+//     if (nextFrom <= 10) {
+//       dispatch(addGroup({ from: nextFrom, to: nextTo }));
+//     }
+//   };
+
+//   const handleShowStatus = async () => {
+//     const allStatuses = await Promise.all(
+//       groups.map(async (group, index) => {
+//         const groupStatuses = {};
+//         for (let i = group.from; i <= group.to; i++) {
+//           const response = await axios.get(`https://jsonplaceholder.typicode.com/todos/${i}`);
+//           groupStatuses[i] = response.data.completed;
+//         }
+//         return { index, statuses: groupStatuses };
+//       })
+//     );
+
+//     allStatuses.forEach(({ index, statuses }) => {
+//       dispatch(setStatuses(index, statuses));
+//     });
+//   };
+
+//   return (
+//     <div className="group-list">
+//       {groups.map((group, index) => (
+//         <div key={index} className="group-container">
+//           <Group index={index} group={group} />
+//         </div>
+//       ))}
+//       <button className="show-status" onClick={handleShowStatus}>Show Status</button>
+//       <button className="add-group" onClick={handleAddGroup}>+ Add Group</button>
+//     </div>
+//   );
+// };
+
+// export default GroupList;
+
+
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { addGroup, setStatuses } from '../store';
 import Group from './Group';
@@ -7,14 +59,20 @@ import axios from 'axios';
 const GroupList = () => {
   const dispatch = useDispatch();
   const groups = useSelector((state) => state.groups);
+  const [allTasksCovered, setAllTasksCovered] = useState(false);
+
+  useEffect(() => {
+    checkAllTasksCovered();
+  }, [groups]);
 
   const handleAddGroup = () => {
     const lastGroup = groups[groups.length - 1];
-    const nextFrom = lastGroup.to + 1;
-    const nextTo = nextFrom + 2 <= 10 ? nextFrom + 2 : 10;
-    if (nextFrom <= 10) {
-      dispatch(addGroup({ from: nextFrom, to: nextTo }));
+    const nextFrom = lastGroup ? lastGroup.to + 1 : 1;
+    if (nextFrom > 10) {
+      return;
     }
+    const nextTo = nextFrom + 2 <= 10 ? nextFrom + 2 : 10;
+    dispatch(addGroup({ from: nextFrom, to: nextTo }));
   };
 
   const handleShowStatus = async () => {
@@ -34,6 +92,16 @@ const GroupList = () => {
     });
   };
 
+  const checkAllTasksCovered = () => {
+    const tasksCovered = new Array(10).fill(false);
+    groups.forEach(group => {
+      for (let i = group.from; i <= group.to; i++) {
+        tasksCovered[i - 1] = true;
+      }
+    });
+    setAllTasksCovered(tasksCovered.every(covered => covered));
+  };
+
   return (
     <div className="group-list">
       {groups.map((group, index) => (
@@ -41,7 +109,13 @@ const GroupList = () => {
           <Group index={index} group={group} />
         </div>
       ))}
-      <button className="show-status" onClick={handleShowStatus}>Show Status</button>
+      <button
+        className="show-status"
+        onClick={handleShowStatus}
+        disabled={!allTasksCovered}
+      >
+        Show Status
+      </button>
       <button className="add-group" onClick={handleAddGroup}>+ Add Group</button>
     </div>
   );
